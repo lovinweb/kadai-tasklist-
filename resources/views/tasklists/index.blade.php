c9bcce41871617b41e308fc42e3eb34f92e539ae
Isaji Kyoichi <zeesdesign@gmail.com>
2017-04-15 09:34:54 +0000

    Complete

---
+++ resources/views/tasklists/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスクリスト一覧</h1>
+
+    @if (count($tasklists) > 0)
+        <ul>
+            @foreach ($tasklists as $tasklist)
+                <li>{!! link_to_route('tasklists.show', $tasklist->id, ['id' => $tasklist->id]) !!} {{ $tasklist->content }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+        {!! link_to_route('tasklists.create', '新規メッセージの投稿', null, ['class' => 'btn btn-primary']) !!}
+
+
+    
+@endsection
