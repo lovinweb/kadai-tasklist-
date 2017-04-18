c149a6f6e6d98757d4539d4ff060f06cecb2e304
Isaji Kyoichi <zeesdesign@gmail.com>
2017-04-18 12:34:23 +0000

    column add v3 & add bootstrap

--- resources/views/tasklists/edit.blade.php
+++ resources/views/tasklists/edit.blade.php
@@ -4,14 +4,27 @@
 
     <h1>id: {{ $tasklist->id }} のタスクリスト編集ページ</h1>
 
+    <div class="row">
+        <div class="col-xs-12 col-md-8 col-lg-6">
+
     {!! Form::model($tasklist, ['route' => ['tasklists.update', $tasklist->id], 'method' => 'put']) !!}
 
-        {!! Form::label('status', 'ステータス:') !!} {!! Form::text('status') !!}
+    <div class="form-group">
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status',null, ['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('content', 'タスクリスト:') !!}
+        {!! Form::text('content',null, ['class' => 'form-control']) !!}
+    </div>
 
-        {!! Form::label('content', 'タスクリスト:') !!} {!! Form::text('content') !!}
-
-        {!! Form::submit('更新') !!}
+        {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
+
+
+
+    </div>
+</div>
 
 @endsection
--- resources/views/tasklists/show.blade.php
+++ resources/views/tasklists/show.blade.php
@@ -2,16 +2,28 @@
 
 @section('content')
 
-    <h1>id = {{ $tasklist->id }} のタスクリスト詳細ページ</h1>
+    <h1>id={{ $tasklist->id }} のタスクリスト詳細ページ</h1>
 
-    <p>ID: {{ $tasklist->id }} </p>
-    <p>ステータス: {{ $tasklist->status }}</p>
-    <p>タスク: {{ $tasklist->content }}</p>
 
-    {!! link_to_route('tasklists.edit', 'このタスクリスト編集', ['id' => $tasklist->id]) !!}
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $tasklist->id }}</td>
+        </tr>
+        <tr>
+            <th>ステータス</th>
+            <td>{{ $tasklist->status }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $tasklist->content }}</td>
+        </tr>
+    </table>
+
+    {!! link_to_route('tasklists.edit', 'このタスク編集', ['id' => $tasklist->id], ['class' => 'btn btn-default']) !!}
     
     {!! Form::model($tasklist, ['route' => ['tasklists.destroy', $tasklist->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
     
     
